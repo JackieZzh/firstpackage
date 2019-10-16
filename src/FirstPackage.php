726cb0ed0bf06130e2625049d41f26371895f256
JackieZzh <862355069@qq.com>
2019-10-16 14:37:19 +0800

    第一个包

---
+++ src/FirstPackage.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/10/16/0016
+ * Time: 13:58
+ */
+
+namespace Jackie;
+
+class FirstPackage{
+    public function Hi()
+    {
+        return "Hi! This's my first time to create composer package!";
+    }
+}
